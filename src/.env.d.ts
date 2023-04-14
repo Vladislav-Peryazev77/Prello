fd55febe5c8bbf8af924ffcb1fb4f76d2772063b
vladislav-peryazev77 <vladperyazev14@gmail.com>
2023-04-14 09:17:01 +0300

    fix: fix another mistake in sentry.ts

--- src/.env.d.ts
+++ src/.env.d.ts
@@ -1,5 +1,5 @@
 interface ImportMetaEnv {
-  readonly VITE_SENTRY_DNS: string;
+  readonly VITE_SENTRY_DSN: string;
   readonly VITE_SUPABASE_API_URL: string;
   readonly VITE_SUPABASE_ANON_KEY: string;
 }

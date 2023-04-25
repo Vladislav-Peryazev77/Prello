0e0c1bfeb0eb966d0f80fb38c0b2bb3824087f74
vladislav-peryazev77 <vladperyazev14@gmail.com>
2023-04-25 22:53:38 +0300

    feat: add component Input and some styles and remove Layout/index.tsx file

--- src/pages/auth/sign-in/index.tsx
+++ src/pages/auth/sign-in/index.tsx
@@ -5,6 +5,7 @@ import { Button } from "@/shared/ui/Button";
 import headerImg from "./header.svg";
 import logomark from "./logomark.svg";
 import styles from "./styles.module.css";
+import { Input } from "@/shared/ui/Input";
 
 interface Props {
   className?: string;
@@ -17,20 +18,21 @@ const SingInPage: FC<Props> = () => {
         <img src={headerImg} alt="Header background" />
       </header>
       <main className={styles.main}>
-        <img className={styles.logomark} src={logomark} alt="Brello logomark" />
+        <img className={styles.logomark} src={logomark} alt="Prello logomark" />
         <section className={styles.section}>
-          <h1 className={styles.headLine}>Sign in</h1>
+          <h1 className={styles.headline}>Sign in</h1>
           <p className={styles.description}>Start your 30-day free trial.</p>
           <form action="#" className={styles.form}>
-            <label className={styles.form}>
-              <span>Email</span>
-              <input
-                className={styles.input}
-                type="email"
-                name="email"
-                placeholder="Enter your email"
-              />
-            </label>
+            {/*<label className={styles.form}>*/}
+            {/*  <span className={styles.span}>Email</span>*/}
+            {/*  <input*/}
+            {/*    className={styles.input}*/}
+            {/*    type="email"*/}
+            {/*    name="email"*/}
+            {/*    placeholder="Enter your email"*/}
+            {/*  />*/}
+            {/*</label>*/}
+            <Input type="email" text="Email" />
             <Button type="submit">Get started</Button>
           </form>
         </section>
